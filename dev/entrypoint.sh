413b3261891def04e82150c00af59fb926082f96
Xabi Ezpeleta <xezpeleta@gmail.com>
2016-10-12 21:03:36 +0000

    Removed debug messages

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -4,8 +4,6 @@ sleep 10
 
 EB_PATH=/usr/local/elkarbackup
 
-echo "MYTEST: $MYSQL_ROOT_PASSWORD"
-
 if [ ! -d "$EB_PATH" ];then
   bash -c "$(curl -s https://gist.githubusercontent.com/xezpeleta/c5a5fe960b39cfab29e935dd381a4ab2/raw/eb-installer.sh)" -s \
     -v dev \
@@ -15,7 +13,7 @@ if [ ! -d "$EB_PATH" ];then
     -U "${MYSQL_ROOT_USER:=root}" \
     -P "${MYSQL_ROOT_PASS:=changeme}" \
     -y \
-    -d \
+    -d
   echo "Restarting apache..."
   service apache2 stop
 fi
