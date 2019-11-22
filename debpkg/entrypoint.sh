2f35083aef14f734703d0e94a4c081677464c7cf
Xabi Ezpeleta <xezpeleta@gmail.com>
2019-11-22 19:38:49 +0100

    Removed multiple package generation

--- debpkg/entrypoint.sh
+++ debpkg/entrypoint.sh
@@ -8,12 +8,6 @@ set -e
 #			repository URL
 #			Add "-b <branch>" if you want to select a custom branch. Example:
 #				REPO="https://github.com/xezpeleta/elkarbackup.git -b fix-issue-79"
-# - $PHP_VERSION
-#			Defaults to PHP_VERSION=5
-#
-# - $EB_VERSION
-#                       Use custom version tag (i.e. EB_VERSION=1.3.0)
-#
 
 DATA_DIR="/data/elkarbackup"
 EXPORT_DIR="/export"
@@ -33,45 +27,6 @@ git clone $GIT_REPO
 
 cd $DATA_DIR
 ./bootstrap.sh
-
-##
-## Use custom version tag for your build
-## Example: EB_VERSION=1.3.0
-##
-if [ ! -z "$EB_VERSION" ];then
-	echo "Updating version number to: $EB_VERSION"
-	echo "- Updating debiancontrol..."
-	debiancontrol=$DATA_DIR/debian/DEBIAN/control
-	currentversion=`sed -n -e '/Version/ s/.*\: *//p' $debiancontrol`
-	sed -i "s/Version:.*/Version: $EB_VERSION/g" $debiancontrol
-
-	echo "- Updating login screen..."
-	loginscreen=$DATA_DIR/src/Binovo/ElkarBackupBundle/Resources/views/Default/login.html.twig
-	sed -i "s/v$currentversion/v$EB_VERSION/g" $loginscreen
-fi
-
-##
-## We need to use different control files for PHP5 and PHP7
-##
-
-if [[ -z "$PHP_VERSION" ]];then
-    php=5
-else
-	if [[ "$PHP_VERSION" == 7 ]];then
-		debiancontrol=$DATA_DIR/debian/DEBIAN/control
-		php7version="-php7"
-		php7depends="Depends: acl, debconf, php, php-cli, php-xml, rsnapshot, mysql-client, php-mysql, sudo, apache2, libapache2-mod-php"
-		echo "Selected PHP7, changing debian/control..."
-
-		# Change Control line
-		currentversion=`sed -n -e '/Version/ s/.*\: *//p' $debiancontrol`
-		sed -i "s/Version:.*/Version: $currentversion$php7version/g" $debiancontrol
-
-		# Change Depends line
-		sed -i "s/Depends:.*/$php7depends/g" $debiancontrol
-	fi
-fi
-
 ./makepackage.sh
 
 DEB_FILE=`ls *deb`
--- debpkg/run.sh
+++ debpkg/run.sh
@@ -5,6 +5,4 @@ docker run --rm --name ebname \
 		 -e UID=$(id -u) \
 		 -e GID=$(id -g) \
 		 -e "GIT_REPO=https://github.com/elkarbackup/elkarbackup.git" \
-		 -e "PHP_VERSION=5" \
-		 -e "EB_VERSION=1.3.0" \
 		 elkarbackup/deb:latest
